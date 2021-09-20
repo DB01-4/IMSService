f7f3a078e26fe10c950572e30d60a2846f88bde9
Jovi Simons <jovisimons009@gmail.com>
2021-09-20 13:22:33 +0200

    Initial commit

---
+++ src/test/kotlin/com/example/imsservice/ImsServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.imsservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ImsServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
