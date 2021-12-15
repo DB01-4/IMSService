ad9e13807fa6a9d69b1694bc4066938a278b8d89
Nino Verhaegh <454777@student.fontys.nl>
2021-12-15 13:45:20 +0100

    added generated value to enable AI

--- src/main/kotlin/com/example/imsservice/entity/Item.java
+++ src/main/kotlin/com/example/imsservice/entity/Item.java
@@ -3,6 +3,7 @@ package com.example.imsservice.entity;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.*;
 
 
 
@@ -12,6 +13,7 @@ import javax.persistence.Table;
 public class Item {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public int id;
     public String name;
     public String type;
